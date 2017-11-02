3f84e37d9563bf4621c73487d97f12d2e12e2463
Bartek Skorulski <bartekskorulski@gmail.com>
2017-11-02 21:10:54 +0100

    inits

---
+++ README.rst
@@ -0,0 +1,9 @@
+Terminator
+==========
+
+Transformations of python's dataframes that I can understand.
+
+----
+
+Well, let's see...
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='Terminator',
+    version='0.0.1dev',
+    packages=['terminator'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.rst').read(),
+)
+
